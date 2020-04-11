35f4e6eb218042858a08bc077cc7a340eafd23f7
whiskeyandwry <starshak@gmail.com>
2020-04-11 14:09:55 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup( name = 'euler_helper',
-      url = 'https://github.com/TomStarshak/euler_helper',
+      url = 'https://github.com/TomStarshak/project-euler-helper',
       author = 'Tom Starshak',
       author_email = 'starshak@gmail.com',
       version = '0.1',
